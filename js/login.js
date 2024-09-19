75f87846b9d1389321c72c92bb983d3c61f83165
Dip <65559847+Dip-Barua@users.noreply.github.com>
2024-09-20 00:50:09 +0600

    Login Entries Verification added using Javascript

---
+++ js/login.js
@@ -0,0 +1,13 @@
+document.getElementById('button-login').addEventListener('click', function(event){
+    event.preventDefault();
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+
+    if(phoneNumber === '1234' && pin-number === '1234'){
+        console.log('User verified');
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('Wrong Username or Pin.')
+    }
+});
